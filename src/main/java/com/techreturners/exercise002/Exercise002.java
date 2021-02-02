8535afccc7961350424874e669f20d0ae2dffcde
Ariel Pillemer <pillemer@gmail.com>
2021-02-02 18:27:28 +0000

    passes checkIsFromManchester and checkCanWatchFilm tests

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,18 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
+    public boolean isFromManchester(Person person) {
+        if (person.getCity().equals("Manchester")) {
+            return true;
+        }
         return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        if (person.getAge() < ageLimit) {
+            return false;
+        }
+        return true;
     }
     
 }
